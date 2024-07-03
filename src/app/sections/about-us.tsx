88aafd9ca32e5d6ed3e6d06e1a4872c645e12e29
Shinn <sunikhedge@gmail.com>
2024-07-03 14:51:20 +0800

    added about us info

--- src/app/sections/about-us.tsx
+++ src/app/sections/about-us.tsx
@@ -1,7 +1,19 @@
 'use client'
+import { useRef } from "react";
 import Marquee from "react-fast-marquee";
+import { useParallax } from "react-scroll-parallax";
+import onScreen from "../hooks/onScreen";
 
 export default function AboutUs(){
+    const ref = useRef(null);
+    const ref2 = useRef(null);
+    const ref3 = useRef(null);
+    const ref4 = useRef(null);
+    const isVisible = onScreen(ref);
+    const isVisible2 = onScreen(ref2);
+    const isVisible3 = onScreen(ref3);
+    const isVisible4 = onScreen(ref4);
+    
     return (
         <div className={`relative overflow-hidden`}>
             <div className={`mt-24 relative w-[110%] h-fit p-12 bg-[#454059] overflow-hidden -translate-x-5 rotate-6 z-20`}>
@@ -11,21 +23,24 @@ export default function AboutUs(){
                 </Marquee>
             </div>
             <div className={`relative w-full h-[150vh] bg-[#454059] flex justify-evenly items-center z-10 -translate-y-24`}>
-                <div className='w-full h-full flex flex-col justify-evenly items-center'>
-                    <div className='flex flex-col w-1/2 items-center'>
-                        <p className='text-4xl font-bold flex text-start'>WHO WE ARE</p>
-                        <p className='text-[#B8A38D] text-justify'>We are a team filled with passionate and determined computer science students based in the Philippines.</p>
+                <div className='w-full h-full flex flex-col justify-evenly items-center translate-y-20'>
+                    <div ref={ref} className='flex flex-col w-1/2 items-center'>
+                        <p className={`text-4xl font-bold flex text-start transition duration-1000 ease-in-out ${isVisible ? '-translate-x-12' : '-translate-x-[200%]'}`}>WHO WE ARE</p>
+                        <p className={`text-[#B8A38D] text-justify transition duration-1000 ease-in-out ${isVisible ? '' : '-translate-x-[200%]'}`}>We are a team filled with passionate and determined computer science students based in the Philippines.</p>
                     </div>
-                    <div>
-                        MEET THE TEAM
+                    <div ref={ref2} className='flex flex-col w-1/2 items-center'>
+                        <p className={`text-4xl font-bold flex text-start transition duration-1000 ease-in-out ${isVisible2 ? '-translate-x-12' : '-translate-x-[200%]'}`}>MEET THE TEAM</p>
+                        <p className={`text-[#B8A38D] text-justify transition duration-1000 ease-in-out ${isVisible2 ? '' : '-translate-x-[200%]'}`}>Antonio Garcia Jr. | Co-Founder | Frontend<br/>Elaijah Sapla | Co-Founder | Backend<br/>Richmond Viloria | Co-Founder | Fullstack</p>
                     </div>
                 </div>
-                <div className='w-full h-full flex flex-col justify-evenly items-center'>
-                    <div>
-                        WHAT WE DO
+                <div className='w-full h-full flex flex-col justify-evenly items-center -translate-y-20'>
+                    <div ref={ref3} className='flex flex-col w-1/2 items-center'>
+                        <p className={`text-4xl font-bold flex text-start transition duration-1000 ease-in-out ${isVisible3 ? 'translate-x-12' : 'translate-x-[200%]'}`}>WHAT WE DO</p>
+                        <p className={`text-[#B8A38D] text-justify transition duration-1000 ease-in-out ${isVisible3 ? '' : 'translate-x-[200%]'}`}>Our company do various software development projects including website applications, mobile applications, gaming application and desktop applications.</p>
                     </div>
-                    <div>
-                        WHAT WE OFFER
+                    <div ref={ref4} className='flex flex-col w-1/2 items-center'>
+                        <p className={`text-4xl font-bold flex text-start transition duration-1000 ease-in-out ${isVisible4 ? 'translate-x-12' : 'translate-x-[200%]'}`}>WHAT WE OFFER</p>
+                        <p className={`text-[#B8A38D] text-justify transition duration-1000 ease-in-out ${isVisible4 ? '' : 'translate-x-[200%]'}`}>We offer -</p>
                     </div>
                 </div>
             </div>
--- src/app/sections/intro.tsx
+++ src/app/sections/intro.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import { RefObject, useRef } from 'react';
 import Marquee from 'react-fast-marquee'
 import { useParallax } from 'react-scroll-parallax';
 
@@ -7,15 +6,19 @@ export default function Intro(){
     const {ref} = useParallax<HTMLDivElement>({
         translateY: [-200, 200]
     })
+
+    const marquee = () => {
+        return <Marquee className='relative w-full h-full -z-10 overflow-hidden' autoFill={true}>
+                <Image src='/image 1.png' alt='placeholder' height={600} width={600} className='blur-sm object-contain'/>
+                <Image src='/image 3.png' alt='placeholder 2' height={400} width={400} className='blur-sm object-contain'/>
+            </Marquee>
+    }
     
     return (
         <div ref={ref} className='w-full h-screen mb-8 flex justify-center items-center -z-20'>
             <Image src='/JOYBREADLOGO.png' alt='joybread studios logo' height={350} width={350} className='absolute'/>
             <div className='relative w-screen h-5/6'>
-                <Marquee className='relative w-full h-full -z-10 overflow-hidden' autoFill={true}>
-                    <Image src='/image 1.png' alt='placeholder' height={600} width={600} className='blur-sm object-contain'/>
-                    <Image src='/image 3.png' alt='placeholder 2' height={400} width={400} className='blur-sm object-contain'/>
-                </Marquee>
+                
             </div>
         </div>
     )
