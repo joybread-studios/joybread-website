204bdbe17dc5de5708181fe767f799eae1832217
Shinn <sunikhedge@gmail.com>
2024-07-03 21:32:26 +0800

    fixed big screen (ithink)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,24 +28,26 @@ export default function Home() {
   }, []);
 
   const [isMobile, setIsMobile] = useState<boolean>(false);
-	const checkIfMobile = () => {
-		if(window.innerWidth < 640) setIsMobile(true);
-		else setIsMobile(false);
-	}
+  const [isBigScreen, setIsBigScreen] = useState<boolean>(false);
+	const checkScreenSize = () => {
+        setIsMobile(window.innerWidth <= 640);
+        setIsBigScreen(window.innerWidth > 1536);
+    }
 
-	useEffect(() => {
-		window.addEventListener('resize', checkIfMobile);
-		return (() => {
-			window.removeEventListener('resize', checkIfMobile);
-		})
-	}, [isMobile]);
+    useEffect(() => {
+        checkScreenSize();
+        window.addEventListener('resize', checkScreenSize);
+        return () => {
+            window.removeEventListener('resize', checkScreenSize);
+        };
+    }, []);
 
 
   return (
     <ParallaxProvider>
       <div className="w-full h-full">
       <div className={`fixed inset-0 flex items-center justify-center bg-black z-50 transition-opacity ease-out duration-1000 ${isLoading ? 'opacity-1' : 'opacity-0 hidden'}`}>
-          <Image src="/JOYBREADLOGO.png" alt="Splash Screen Image" height={isMobile ? 350 : 250} width={isMobile ? 350 : 250} className={`transition-opacity ease-out duration-500 ${imageLoaded ? 'opacity-1' : 'opacity-0'}`}/>
+          <Image src="/JOYBREADLOGO.png" alt="Splash Screen Image" height={!isMobile ? (!isBigScreen ? 350 : 600) : 250} width={!isMobile ? (!isBigScreen ? 350 : 600) : 250} className={`transition-opacity ease-out duration-500 ${imageLoaded ? 'opacity-1' : 'opacity-0'}`}/>
       </div>
       <Header />
       <Intro />
